1fd0cdb275d37e9cd28a22d209bab23c2cacd08e
N31k <n31k0s@gmail.com>
2018-01-16 22:47:11 +0200

    looking into ng-change

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -4,6 +4,12 @@ myApp.controller('LunchController', ['$scope', function ($scope){
 		console.log("Hello World from LunchController");
 		$scope.lunchMenu = "list comma separated dishes you usually have for lunch"
 		$scope.message = "How much was that you filthy pig?";
+	    if ($scope.lunchMenu != ''){
+			var re = /\s?,+\s?/;
+			var new_string = $scope.lunchMenu.split(re);
+			console.log(new_string);
+			$scope.lunchMenu = new_string;
+		}
 
 	}])
 
