17716d0f2782a79cd14d54d4d7da62c3a7f83680
N31k <n31k0s@gmail.com>
2018-01-17 02:07:47 +0200

    introduced watch functionality

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,15 +2,15 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('LunchController', ['$scope', function ($scope){
 		console.log("Hello World from LunchController");
-		$scope.lunchMenu = "list comma separated dishes you usually have for lunch"
-		$scope.message = "How much was that you filthy pig?";
-	    if ($scope.lunchMenu != ''){
+		$scope.$watch('lunchMenu', function() {
+			console.log('form model has been changed');
 			var re = /\s?,+\s?/;
 			var new_string = $scope.lunchMenu.split(re);
 			console.log(new_string);
-			$scope.lunchMenu = new_string;
-		}
+			$scope.message = new_string;
 
-	}])
+		}, true);
+
+		}])
 
 
