1a6501d58258bb25871cb6fa914b0b49cfe73289
N31k <n31k0s@gmail.com>
2018-01-17 02:45:08 +0200

    Corrections

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,22 +2,22 @@ var myApp = angular.module('myApp',[]);
 
 myApp.controller('LunchController', ['$scope', function ($scope){
 		console.log("Hello World from LunchController");
+	    $scope.lunchMenu = '';
+	    $scope.message = 'Enter some items!';
 		$scope.$watch('lunchMenu', function() {
 			console.log('form model has been changed');
 			var re = /\s?,+\s?/;
 			var new_string = $scope.lunchMenu.split(re);
-			console.log(new_string);
+			new_string = new_string.filter(function(word) word != '');
 			var len = new_string.length;
-			if (new_string[0] != '' && len < 3){
+			console.log(new_string);
+			if (len < 3){
 			  $scope.message = 'Enjoy!';
 			}
-			if (new_string[0] != '' && len > 3){
+			if (len > 3){
 			  $scope.message = 'Too much!';
 					} 
-			if (new_string[0] == ''){
-			  $scope.message = 'Enter some foodstuffs!';
-					} 
-
+		
 		}, true);
 
 		}])
